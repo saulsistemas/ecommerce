f58c44982bddbf0d8fbaa6cf64b4ec4675b1b356
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-11-12 14:41:17 -0500

    avance hasta carrito

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,5 +29,7 @@ class DatabaseSeeder extends Seeder
         $this->call(ColorProductSeeder::class);
         $this->call(SizeSeeder::class);
         $this->call(ColorSizeSeeder::class);
+
+        $this->call(DepartmentSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Livewire\CreateOrder;
 use App\Http\Livewire\ShoppingCart;
+
+//use App\Http\Livewire\PaymentOrder;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,11 +31,11 @@ Route::middleware(['auth'])->group(function(){
 
    // Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
 
-   // Route::get('orders/create', CreateOrder::class)->name('orders.create');
+    Route::get('orders/create', CreateOrder::class)->name('orders.create');
 
-   // Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+    Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
 
-   // Route::get('orders/{order}/payment', PaymentOrder::class)->name('orders.payment');
+   //Route::get('orders/{order}/payment', PaymentOrder::class)->name('orders.payment');
 
    // Route::get('orders/{order}/pay', [OrderController::class, 'pay'])->name('orders.pay');
 
@@ -39,6 +43,3 @@ Route::middleware(['auth'])->group(function(){
 
 });
 
-Route::get('prueba',function(){
-   \Cart::destroy();
-});
